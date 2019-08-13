e5d60c6d8414f601060dc1fcb8da7b4b5a645dbc
Laney McKee <laneymckeephotography@gmail.com>
2019-08-13 13:11:44 -0500

    completed base content

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+//Every component needs this import
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Phone: 1-800-WE-REACT</p>
+        <p>Email: info@ReactActors.com</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+//Every component needs this import
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div class="App-footer">
+        <p>© React Actors</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
